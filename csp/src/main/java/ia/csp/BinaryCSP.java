c80a83f0aa57f697cba934bfdc4e7c1f746ae2de
nourch14 <nourch14@b032-05.crio.dauphine.fr>
2017-11-24 13:37:20 +0100

    i try so hard

--- csp/src/main/java/ia/csp/BinaryCSP.java
+++ csp/src/main/java/ia/csp/BinaryCSP.java
@@ -28,11 +28,27 @@ public class BinaryCSP<D> {
 
 
     public boolean forwardCheck() {
+        for (BinaryConstraint bc:constraints //delete impossible values
+                ) {
+            if (bc.getX().isAssigned()) {
+                bc.getY().removePossibleValue(bc.getX().getValue());
+            }
+            if (bc.getY().isAssigned()) {
+                bc.getX().removePossibleValue(bc.getY().getValue());
+            }
+            if (bc.getY().getPossibleValues().size() == 0 || bc.getX().getPossibleValues().size() == 0) {
+                return false;//????
+            }
+        }
+        return fccore();
+    }
+
+    public boolean fccore(){
 
         boolean result=false;
         Variable courant=vars.get(0);
         int index=0;
-        while(courant.isAssigned() && index<vars.size()){
+        while(courant.isAssigned() && index<vars.size()){//choice of the first variable
             courant=vars.get(index);
             index++;
             if(index==vars.size()){
@@ -40,7 +56,7 @@ public class BinaryCSP<D> {
             }
         }
 
-        for (BinaryConstraint bc:constraints
+        for (BinaryConstraint bc:constraints //delete impossible values
              ) {
             if(bc.getX().isAssigned()){
                 bc.getY().removePossibleValue(bc.getX().getValue());
@@ -49,79 +65,97 @@ public class BinaryCSP<D> {
                 bc.getX().removePossibleValue(bc.getY().getValue());
             }
             if(bc.getY().getPossibleValues().size()==0 || bc.getX().getPossibleValues().size()==0){
+                //System.out.println("nanananan");
+                courant.reset();
+                for (Variable varr:vars) {
+                    if(!varr.isAssigned()){
+                        varr.resetPossibleValues();
+                    }
+                }
                 return false;//????
             }
         }
 
         for (Object o:courant.getPossibleValues()) {
+
             courant.setValue(o);
             for (BinaryConstraint bc: constraints) {
+                //System.out.println("pb="+bc.getX().getName()+" "+bc.getX().getPossibleValues()+
+                        //" et "+bc.getX().getName()+" "+bc.getY().getPossibleValues());
                 if(bc.getX().equals(courant)){
-                    bc.getY().removePossibleValue(bc.getX().getValue());
+                    bc.getY().removePossibleValue(o);
                 }
                 if(bc.getY().equals(courant)){
-                    bc.getX().removePossibleValue(bc.getY().getValue());
+                    bc.getX().removePossibleValue(o);
+                }
+                if(bc.getY().getPossibleValues().size()==0 || bc.getX().getPossibleValues().size()==0){
+                    System.out.println("on s arrete");
+                    courant.reset();
+                    for (Variable varr:vars) {
+                        if(!varr.isAssigned()){
+                            varr.resetPossibleValues();
+                        }
+                    }
+                    for (BinaryConstraint bc1:constraints //delete impossible values
+                            ) {
+                        //System.out.println("boucle1");
+                        if(bc1.getX().isAssigned()){
+                            bc1.getY().removePossibleValue(bc1.getX().getValue());
+                        }
+                        if(bc1.getY().isAssigned()){
+                            bc1.getX().removePossibleValue(bc1.getY().getValue());
+                        }
+                        if(bc1.getY().getPossibleValues().size()==0 || bc1.getX().getPossibleValues().size()==0){
+                            courant.reset();
+                            for (Variable varr:vars) {
+                                if(!varr.isAssigned()){
+                                    varr.resetPossibleValues();
+                                }
+                            }
+                            return false;//????
+                        }
+                    }
+                    return false;//????
                 }
             }
-            for (Variable var:vars
+            /*for (Variable var:vars
                  ) {
                 System.out.println(var.getName()+","+var.getPossibleValues());
-                System.out.println(" ");
-            }
+            }*/
+            System.out.println(this);
 
             result=forwardCheck();
+            if(result){
+                return true;
+            }
+            System.out.println("on reset"+courant.getName());
             courant.reset();
             for (Variable varr:vars) {
                 if(!varr.isAssigned()){
                     varr.resetPossibleValues();
                 }
             }
-
-            if(result){
-                return true;
+            for (BinaryConstraint bc:constraints //delete impossible values
+                    ) {
+                //System.out.println("boucle2");
+                if(bc.getX().isAssigned()){
+                    bc.getY().removePossibleValue(bc.getX().getValue());
+                }
+                if(bc.getY().isAssigned()){
+                    bc.getX().removePossibleValue(bc.getY().getValue());
+                }
+                if(bc.getY().getPossibleValues().size()==0 || bc.getX().getPossibleValues().size()==0){
+                    courant.reset();
+                    for (Variable varr:vars) {
+                        if(!varr.isAssigned()){
+                            varr.resetPossibleValues();
+                        }
+                    }
+                    return false;//????
+                }
             }
-           /* System.out.println("coucou");
-            return false;*/
+            //return false;
         }
-
-        /*List<BinaryCSP> list = new ArrayList<BinaryCSP>();
-
-        list.add(this);
-        boolean result = false;
-        //int index =0;
-
-
-        BinaryCSP<D> binaryCSP=new BinaryCSP<D>();
-        binaryCSP.vars=list.get(list.size()-1).vars;
-        binaryCSP.constraints=list.get(list.size()-1).constraints;
-
-        for (BinaryConstraint b:binaryCSP.constraints) {//update the constraints
-            if(b.getX().isAssigned() || b.getY().isAssigned()){
-                revise(b);
-            }
-            if(b.getX().getPossibleValues().size()==0 || b.getY().getPossibleValues().size()==0){
-                System.out.println("false");
-                //return false;   //if the sudoku is not possible
-            }
-        }
-
-        while(binaryCSP.vars.get(index).isAssigned()) {   //find a first variable
-            System.out.println(binaryCSP.vars.get(index));
-            index++;
-        }
-        if(index==binaryCSP.vars.size()){
-            System.out.println("true");
-            return true;
-        }
-
-        for (D d: binaryCSP.vars.get(index).getPossibleValues()) {
-            //System.out.println("d"+d);
-
-            binaryCSP.vars.get(index).setValue(d);
-            System.out.println(binaryCSP);
-            //forwardCheck();
-
-        }*/
 
         return false;
     }
